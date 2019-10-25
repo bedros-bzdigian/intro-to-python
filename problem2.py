df8878709aa482e681576e704ba4f1bd72628c5e
bedros bzdigian <bedig-19962@hotmail.com>
2019-10-25 06:19:48 -0700

    please run this program

---
+++ problem2.py
@@ -0,0 +1,10 @@
+course = "intro-to-python"
+student = "Bedros Bzdigian"
+nickname = "Engineer"
+grade = 10
+python_expert = False
+print ("course:", course)
+print ("student:", student)
+print ("nickname:" , nickname)
+print ("grade:", grade)
+print ("python_expert", python_expert)
---
+++ problem4.py
@@ -0,0 +1,6 @@
+AB = 3
+AC = 4
+BC = AC**2 + AB**2
+BC = BC**0.5 
+BC = int (BC)
+print (BC)
