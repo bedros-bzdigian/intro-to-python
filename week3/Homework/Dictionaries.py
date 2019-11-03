fb6d1a04005f6bdc309a034c64d6e36693d746f8
bedros bzdigian <bedig-19962@hotmail.com>
2019-11-03 10:56:51 -0800

    ok

---
+++ week3/Homework/Dictionaries.py
@@ -0,0 +1,5 @@
+market = {"Dairy":["yogurt" , "cheese"] , "fruits" : ["bannana" , "apple" , "orange" , "lemon" , "apple" , "bannana" , "bannana"]}
+print ("The dictionary market before changes: " , market)
+market["candies"] = ["mars" , "kinder" , "twix"]
+market["fruits"].sort()
+print ("The dictionary market after changes: " , market)
