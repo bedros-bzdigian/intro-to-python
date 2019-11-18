d2a35dd7de587168c6b2ed9dc2540c921ef33798
bedros bzdigian <bedig-19962@hotmail.com>
2019-11-18 08:29:05 -0800

    hi

---
+++ week5/pratical/problem7/calc.py
@@ -0,0 +1,19 @@
+from pretty_print import simple_print()
+
+
+from pretty_print import pro_print ()
+
+
+def calculate_cube(x):
+    return x*x*x
+
+def calculate_square(x):
+    return x*x 
+
+def main():
+    y = calculate_square(2)
+    simple_print(y)
+    f = calculate_cube(4)
+    pro_print(f)
+
+main()
